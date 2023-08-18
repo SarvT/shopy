2a9194622d64858994931c854734f74de6dc9b3c
SarvT <91484197+SarvT@users.noreply.github.com>
2023-08-18 09:03:29 +0530

    initialization with a basic setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className='navbar'>
+            <div className="logo">
+                Nav
+            </div>
+            <div className="nav-text">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
